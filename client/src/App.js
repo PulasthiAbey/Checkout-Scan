80e37104fbc9b47e11d1741f428ae8279df625e3
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2024-01-29 03:50:49 +0530

    Cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,23 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import mondaySdk from "monday-sdk-js";
-import "monday-ui-react-core/dist/main.css";
-//Explore more Monday React Components here: https://style.monday.com/
 import AttentionBox from "monday-ui-react-core/dist/AttentionBox.js";
 
-// Usage of mondaySDK example, for more information visit here: https://developer.monday.com/apps/docs/introduction-to-the-sdk/
+import "./App.css";
+import "monday-ui-react-core/dist/main.css";
+
 const monday = mondaySdk();
 
 const App = () => {
   const [context, setContext] = useState();
 
   useEffect(() => {
-    // Notice this method notifies the monday platform that user gains a first value in an app.
-    // Read more about it here: https://developer.monday.com/apps/docs/mondayexecute#value-created-for-user/
     monday.execute("valueCreatedForUser");
 
-    // TODO: set up event listeners, Here`s an example, read more here: https://developer.monday.com/apps/docs/mondaylisten/
     monday.listen("context", (res) => {
       setContext(res.data);
     });
   }, []);
 
-  //Some example what you can do with context, read more here: https://developer.monday.com/apps/docs/mondayget#requesting-context-and-settings-data
   const attentionBoxText = `Hello, your user_id is: ${
     context ? context.user.id : "still loading"
   }.
